7c1972d9db0743a207698511e67510c4809b90c9
Pete LePage <pete.lepage@pobox.com>
2013-07-05 18:15:47 -0400

    Added commands for start_activity, get_activity and turn_off

--- harmony/client.py
+++ harmony/client.py
@@ -45,6 +45,62 @@ class HarmonyClient(sleekxmpp.ClientXMPP):
         device_list = action_cmd.text
         return json.loads(device_list)
 
+    def get_current_activity(self):
+        """Retrieves the current activity.
+
+        Returns:
+          A int with the activity ID.
+        """
+        iq_cmd = self.Iq()
+        iq_cmd['type'] = 'get'
+        action_cmd = ET.Element('oa')
+        action_cmd.attrib['xmlns'] = 'connect.logitech.com'
+        action_cmd.attrib['mime'] = (
+            'vnd.logitech.harmony/vnd.logitech.harmony.engine?getCurrentActivity')
+        iq_cmd.set_payload(action_cmd)
+        result = iq_cmd.send(block=True)
+        payload = result.get_payload()
+        assert len(payload) == 1
+        action_cmd = payload[0]
+        assert action_cmd.attrib['errorcode'] == '200'
+        activity = action_cmd.text.split("=")
+        return int(activity[1])
+
+    def start_activity(self, activity_id):
+        """Starts an activity.
+
+        Args:
+            activity_id: An int or string identifying the activity to start
+
+        Returns:
+          A nested dictionary containing activities, devices, etc.
+        """
+        iq_cmd = self.Iq()
+        iq_cmd['type'] = 'get'
+        action_cmd = ET.Element('oa')
+        action_cmd.attrib['xmlns'] = 'connect.logitech.com'
+        action_cmd.attrib['mime'] = ('harmony.engine?startactivity')
+        cmd = 'activityId=' + str(activity_id) + ':timestamp=0'
+        action_cmd.text = cmd
+        iq_cmd.set_payload(action_cmd)
+        result = iq_cmd.send(block=True)
+        payload = result.get_payload()
+        assert len(payload) == 1
+        action_cmd = payload[0]
+        return action_cmd.text
+
+    def turn_off(self):
+        """Turns the system off if it's on, otherwise it does nothing.
+
+        Returns:
+          True.
+        """
+        activity = self.get_current_activity()
+        print activity
+        if activity != -1:
+            print "OFF"
+            self.start_activity(-1)
+        return True
 
 def create_and_connect_client(ip_address, port, token):
     """Creates a Harmony client and initializes session.
